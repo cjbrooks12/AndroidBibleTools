8969462d9ad0eede3fbea1329e5cb24f1b17993a
Casey <cjbrooks12@gmail.com>
2015-03-05 00:59:17 -0600

    Documented AbstractVerse and generated project-wide javadoc in the /docs directory

--- app/src/main/java/com/caseybrooks/androidbibletools/basic/AbstractVerse.java
+++ app/src/main/java/com/caseybrooks/androidbibletools/basic/AbstractVerse.java
@@ -2,8 +2,8 @@ package com.caseybrooks.androidbibletools.basic;
 
 import com.caseybrooks.androidbibletools.data.Formatter;
 import com.caseybrooks.androidbibletools.data.Metadata;
-import com.caseybrooks.androidbibletools.data.Reference;
 import com.caseybrooks.androidbibletools.data.OnDownloadListener;
+import com.caseybrooks.androidbibletools.data.Reference;
 import com.caseybrooks.androidbibletools.defaults.DefaultFormatter;
 import com.caseybrooks.androidbibletools.enumeration.Version;
 
@@ -14,8 +14,13 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.TreeSet;
 
+/**An abstract implementation of a Verse in the Bible. A verse represents a location
+ * and its text, and is considered immutable in that the location the verse
+ * points to is fixed. A verse contains several peripheral classes to give a
+ * verse metadata, provide intelligent sorting, and create a consistent IO pattern.
+ */
 public abstract class AbstractVerse implements Comparable<AbstractVerse> {
-	//Data Members
+//Data Members
 //------------------------------------------------------------------------------
 	protected Version version;
     protected final Reference reference;
@@ -24,6 +29,13 @@ public abstract class AbstractVerse implements Comparable<AbstractVerse> {
 	protected TreeSet<Tag> tags;
 	protected OnDownloadListener listener;
 
+	/**
+	 * Constructor for AbstractVerse that takes a Reference object as initialization
+	 *
+	 * @param reference the Reference that this verse points to in the Bible
+	 *
+	 * @see com.caseybrooks.androidbibletools.data.Reference
+	 */
 	public AbstractVerse(Reference reference) {
 		this.version = Version.KJV;
         this.reference = reference;
@@ -32,26 +44,110 @@ public abstract class AbstractVerse implements Comparable<AbstractVerse> {
 		this.tags = new TreeSet<>();
 	}
 
+	/**
+	 * Constructor for AbstractVerse that takes a parses a String for a reference
+	 * and creates the Reference if it is able to correctly parse the string
+	 *
+	 * @param reference the Reference that this verse points to in the Bible
+	 *
+	 * @throws ParseException if the String could not be parsed
+	 *
+	 * @see com.caseybrooks.androidbibletools.data.Reference
+	 */
     public AbstractVerse(String reference) throws ParseException {
         this.version = Version.KJV;
         this.reference = Reference.parseReference(reference);
         this.formatter = new DefaultFormatter();
         this.metadata = new Metadata();
         this.tags = new TreeSet<>();
-    }
+	}
 
 //Defined methods
 //------------------------------------------------------------------------------
-    public Version getVersion() { return version; }
-    public void setVersion(Version version) { this.version = version; }
-    public Reference getReference() { return reference; }
-    public Formatter getFormatter() { return formatter; }
-    public void setFormatter(Formatter formatter) { this.formatter = formatter; }
-    public Metadata getMetadata() { return metadata; }
-    public void setMetadata(Metadata metadata) { this.metadata = metadata; }
-	public OnDownloadListener getOnDownloadListener() { return listener; }
-	public void setOnDownloadListener(OnDownloadListener listener) { this.listener = listener; }
 
+	/**
+	 * Get the currently set Version of this verse
+	 * @return {@link com.caseybrooks.androidbibletools.enumeration.Version}
+	 *
+	 */
+	public Version getVersion() {
+		return version;
+	}
+
+	/**
+	 * Set the Version of this verse
+	 * @param version the desired Bible translation
+	 *
+	 * @see com.caseybrooks.androidbibletools.enumeration.Version
+	 */
+    public void setVersion(Version version) {
+		this.version = version;
+	}
+
+	/**
+	 * Get the Reference this verse points to
+	 *
+	 * @return {@link com.caseybrooks.androidbibletools.data.Reference}
+	 */
+    public Reference getReference() {
+		return reference;
+	}
+
+	/**
+	 * Get the Formatter used to display this verse's text
+	 *
+	 * @return {@link com.caseybrooks.androidbibletools.data.Formatter}
+	 *
+	 * @see com.caseybrooks.androidbibletools.defaults.DefaultFormatter
+	 */
+    public Formatter getFormatter() {
+		return formatter;
+	}
+
+	/**
+	 * Set the Formatter to be used when printing this verse with @link getText()
+	 *
+	 * @param formatter the Formatter to be used
+	 *
+	 * @see com.caseybrooks.androidbibletools.data.Formatter
+	 * @see com.caseybrooks.androidbibletools.defaults.DefaultFormatter
+	 */
+    public void setFormatter(Formatter formatter) {
+		this.formatter = formatter;
+	}
+
+	/**
+	 * Get the HashMap of arbitrary metadata associated with this verse
+	 *
+	 * @return {@link com.caseybrooks.androidbibletools.data.Metadata}
+	 */
+    public Metadata getMetadata() { return metadata; }
+
+	/**
+	 * Set the metadata to be associated with this verse
+	 *
+	 * @param metadata the MetaData object to set
+	 *
+	 * @see com.caseybrooks.androidbibletools.data.Metadata
+	 */
+    public void setMetadata(Metadata metadata) {
+		this.metadata = metadata;
+	}
+
+	/**
+	 * Register a callback to notify the start and end of downloading the text
+	 * of this verse
+	 * @param listener the callback that will run
+	 *
+	 * @see com.caseybrooks.androidbibletools.data.OnDownloadListener
+	 */
+	public void setOnDownloadListener(OnDownloadListener listener) {
+		this.listener = listener;
+	}
+
+//Tags
+//TODO: make Tags its own class
+//------------------------------------------------------------------------------
 	public AbstractVerse removeAllTags() {
         tags.clear();
 
@@ -88,14 +184,68 @@ public abstract class AbstractVerse implements Comparable<AbstractVerse> {
 
 //Abstract Methods
 //------------------------------------------------------------------------------
+
+	/**
+	 * Get a human-readable text representation of this verse using the set
+	 * {@link com.caseybrooks.androidbibletools.data.Formatter}. Default formatters
+	 * can display the text in various ways designed for memorization, but custom
+	 * Formatters can be configured for any kind of output, including HTML, XML, or JSON
+	 *
+	 * @return {@link java.lang.String}
+	 *
+	 * @see #setFormatter(com.caseybrooks.androidbibletools.data.Formatter)
+	 */
 	public abstract String getText();
+
+	/**
+	 * Get a URL to this verse on BibleStudyTools.com
+	 *
+	 * @return {@link java.lang.String} the URL of this verse on BibleStudyTools.com
+	 */
     public abstract String getURL();
+
+	/**
+	 * Download the verse from the internet. Should always be called from a non-UI
+	 * thread, as it must access the internet which will throw an exception if
+	 * run on the UI thread.
+	 *
+	 * @return {@link com.caseybrooks.androidbibletools.basic.AbstractVerse} this verse object
+	 * @throws IOException
+	 */
 	public abstract AbstractVerse retrieve() throws IOException;
+
+	/**
+	 * Returns a String XML representation of this verse. Is equivalent to calling
+	 * #toXML() and transforming the result to a String.
+	 * @return
+	 */
 	public abstract String toXMLString();
+
+	/**
+	 * Generates an XML representation of this verse, designed to create a consistent
+	 * IO sharing pattern
+	 *
+	 * @param doc the base Document to add this verse as a Node to
+	 * @return {@link org.w3c.dom.Element} a single element of an XML file. May
+	 * be kept as the root or added to another element
+	 */
 	public abstract Element toXML(Document doc);
 
 //Comparison methods
 //------------------------------------------------------------------------------
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @param verse the verse to compare this to
+	 * @return the int result of comparison
+	 */
 	public abstract int compareTo(AbstractVerse verse);
+
+	/**
+	 * {@inheritDoc}
+	 * @param verse the cerse to compare this to
+	 * @return the boolean result of checking for equality
+	 */
 	public abstract boolean equals(AbstractVerse verse);
 }
